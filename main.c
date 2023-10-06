1431233dce2e495ede4c5805380ea78f4671412e
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-10-07 00:26:05 +0300

    update 1

--- main.c
+++ main.c
@@ -1,68 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include "main.h"
 
-#define MAX_INPUT_SIZE 100
+/**
+ * main - The main entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *input = (char *)malloc(MAX_INPUT_SIZE);
+	size_t input_size = MAX_INPUT_SIZE;
+	ssize_t line_length;
 
-void execute_commands(char *input);
+	if (input == NULL)
+	{
+		perror("malloc");
+		return (EXIT_FAILURE);
+	}
 
-int main(int argc, char *argv[]) {
-    char *input = (char *)malloc(MAX_INPUT_SIZE);
-    size_t input_size = MAX_INPUT_SIZE;
-    ssize_t line_length;
+	while (1)
+	{
+		write(STDOUT_FILENO, "$ ", 2); /* Print the shell prompt */
 
-    if (input == NULL) {
-        perror("malloc");
-        return EXIT_FAILURE;
-    }
+		line_length = getline(&input, &input_size, stdin);
 
-    while (1) {
-        write(STDOUT_FILENO, "$ ", 2); // Print the shell prompt
+		if (line_length == -1)
+		{
+			/* Handle end of file (Ctrl+D) */
+			write(STDOUT_FILENO, "\n", 1);
+			free(input);
+			break;
+		}
 
-        line_length = getline(&input, &input_size, stdin);
+		input[line_length - 1] = '\0'; /* Remove newline character */
+		execute_commands(input);
+	}
 
-        if (line_length == -1) {
-            // Handle end of file (Ctrl+D)
-            write(STDOUT_FILENO, "\n", 1);
-            free(input);
-            break;
-        }
-
-        input[line_length - 1] = '\0'; // Remove newline character
-        execute_commands(input);
-    }
-
-    free(input);
-    return 0;
-}
-
-void execute_commands(char *input) {
-    // Tokenize the input to separate the command and its arguments
-    char *command = strtok(input, " ");
-
-    if (command == NULL) {
-        return; // Empty line, do nothing
-    }
-
-    pid_t child_pid = fork();
-
-    if (child_pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if (child_pid == 0) {
-        // Child process
-        char *envp[] = { NULL }; // You can populate envp with environment variables if needed
-
-        if (execve(command, NULL, envp) == -1) { // Execute the command
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        // Parent process
-        wait(NULL); // Wait for the child process to finish
-    }
+	free(input);
+	return (0);
 }
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 100
+
+void execute_commands(char *input);
+int main(void);
+
+#endif /* MAIN_H */
